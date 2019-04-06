67c65b10b9e7bb6aaeafeea140c4ec3ace0b55d9
Carlos Guerrero <guerreroc47@gmail.com>
2019-04-05 23:37:06 -0700

    login
    I am still working on it. I am at camp pondo

---
+++ sfh_app/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:sfh_app/main.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        title: 'Flutter login demo',
+        debugShowCheckedModeBanner: false,
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: new RootPage(auth: new Auth()));
+  }
+}
