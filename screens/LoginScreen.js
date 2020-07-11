271872a72aa28e7c6da087a8b73aadccaa6c6b11
Carlos <guerreroc47@gmail.com>
2020-07-10 21:52:13 -0700

    created the stylesheet and added the features

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,20 +1,30 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, TextInput } from "react-native";
 
 const LoginScreen = (props) => {
   return (
     <View style={styles.screen}>
-      <Text>Login Screen</Text>
+      <Text>Logo (Sign for Humanity)</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Username"
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Password"
+          style={styles.input}
+        />
+      </View>
+      <Button
+        title="Submit"
+        onPress={() => {
+          props.navigation.navigate({ routeName: "App" });
+        }}
+      />
       <Button
         title="Sign up"
         onPress={() => {
           props.navigation.navigate({ routeName: "Signup" });
-        }}
-      />
-      <Button
-        title="Log in"
-        onPress={() => {
-          props.navigation.navigate({ routeName: "App" });
         }}
       />
     </View>
@@ -26,6 +36,22 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "#F8F8F8",
+    padding: 20,
+  },
+  inputContainer: {
+    justifyContent: "space-between",
+    alignItems: "stretch",
+    margin: 5,
+  },
+  input: {
+    width: 225,
+    height: 45,
+    borderWidth: 1,
+    padding: 10,
+    margin: 10,
+    borderRadius: 45,
+    backgroundColor: "#ECECEC",
   },
 });
 
