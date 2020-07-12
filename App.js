5edfa0ab2c9fda61dfc3f400cd9f574b05815da4
Shane <shanewilsonpro@gmail.com>
2020-07-11 18:20:36 -0700

    firebase set up and successfully receive user data

--- App.js
+++ App.js
@@ -1,9 +1,22 @@
 import React from "react";
 import { enableScreens } from "react-native-screens";
 import AppContainer from "./navigation";
+import * as firebase from "firebase/app";
+import { firebaseConfig } from "./Config/config";
 
 enableScreens();
 
-export default function App() {
-  return <AppContainer />;
+class App extends React.Component {
+  constructor() {
+    super();
+    this.initializeFirebase();
+  }
+  initializeFirebase = () => {
+    firebase.initializeApp(firebaseConfig);
+  };
+  render() {
+    return <AppContainer />;
+  }
 }
+
+export default App;
---
+++ Config/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCCH0JpPb-7i5P_ix-LH-vtubh7SOF_HXo",
+    authDomain: "sfh-mobile-app.firebaseapp.com",
+    databaseURL: "https://sfh-mobile-app.firebaseio.com",
+    projectId: "sfh-mobile-app",
+    storageBucket: "sfh-mobile-app.appspot.com",
+    messagingSenderId: "443991699430",
+    appId: "1:443991699430:web:f62f35079bb93788a9f027",
+    measurementId: "G-NXM53NV1PR"
+  };
