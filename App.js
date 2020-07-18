099372af4b9784fb6e8338ece73246203c33c2c9
Shane <shanewilsonpro@gmail.com>
2020-07-18 11:57:28 -0700

    fixes federated facebook login using auth session and fix/add minor things

--- App.js
+++ App.js
@@ -1,53 +1,52 @@
 import React from "react";
 import { enableScreens } from "react-native-screens";
 import AppContainer from "./navigation";
-import * as firebase from "firebase/app";
-import { firebaseConfig } from "./Config/config";
 import { AppLoading } from "expo";
-import * as Font from 'expo-font';
-
-
-
-//firebase.initializeApp(firebaseConfig);
-
+import * as Font from "expo-font";
+import { firebaseConfig } from "./Config/config";
+import firebase from "firebase";
 
 enableScreens();
 
 export default class App extends React.Component {
+  _isMounted = false;
+
   //Custom fonts
-
-
-  //Setup Firebase
-  //constructor() {
-  //  super();
-  //  this.initializeFirebase();
-  //}
-
   state = {
     fontLoaded: false,
   };
 
-  async componentDidMount() {
-      await Font.loadAsync({
-        "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
-        "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
-        "alfaSlabOne": require("./assets/fonts/AlfaSlabOne-Regular.ttf"),
-      });
-      this.setState({ fontLoaded: true});
-    }
+  constructor() {
+    super();
+    this.initializeFirebase();
+  }
 
-  //initializeFirebase = () => {
-  //  firebase.initializeApp(firebaseConfig);
-  //};
+  async componentDidMount() {
+    this._isMounted = true;
+    await Font.loadAsync({
+      "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+      "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+      alfaSlabOne: require("./assets/fonts/AlfaSlabOne-Regular.ttf"),
+    });
+    if (this._isMounted) {
+      this.setState({ fontLoaded: true });
+    }
+  }
+
+  initializeFirebase = () => {
+    firebase.initializeApp(firebaseConfig);
+  };
+
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
 
   render() {
-    const {fontLoaded} = this.state;
+    const { fontLoaded } = this.state;
 
     if (fontLoaded) {
       return <AppContainer />;
     }
-      return <AppLoading />;
+    return <AppLoading />;
   }
 }
-
-//export default App;
