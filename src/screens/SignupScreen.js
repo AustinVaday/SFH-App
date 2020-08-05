8099a349c96155c8f314f29bfad18f5dedcf968f
Carlos <guerreroc47@gmail.com>
2020-08-04 21:44:49 -0700

    set up the component for firebase

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,124 +1,160 @@
-import React from "react";
+import React, { Component } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { TextInput } from "react-native-paper";
 import Colors from "../constants/Colors";
 import { Button } from "react-native-elements";
 import { LinearGradient } from "expo-linear-gradient";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import * as firebase from "firebase/app";
+import "firebase/auth";
 
-const SignupScreen = (props) => {
-  return (
-    <View style={styles.screen}>
-      <View style={styles.textsStyle}>
-        <Text style={styles.text1}>Create Account</Text>
-        <Text style={styles.text2}>Sign up to get started!</Text>
-      </View>
-      <View>
-        <TextInput
-          style={{
-            justifyContent: "center",
-            width: 340,
-            height: 60,
-            fontSize: 18,
-            margin: 5,
-          }}
-          mode="outlined"
-          label="Full Name"
-          theme={{
-            roundness: 15,
-            colors: {
-              primary: Colors.primaryColor,
-              nderlineColor: "blue",
-              placeholder: "#cecbce",
-              background: Colors.secondaryColor,
-            },
-          }}
-        />
-      </View>
-      <View>
-        <TextInput
-          style={{
-            justifyContent: "center",
-            width: 340,
-            height: 60,
-            fontSize: 18,
-            margin: 5,
-          }}
-          mode="outlined"
-          label="Email"
-          theme={{
-            roundness: 15,
-            colors: {
-              primary: Colors.primaryColor,
-              nderlineColor: "blue",
-              placeholder: "#cecbce",
-              background: Colors.secondaryColor,
-            },
-          }}
-        />
-      </View>
-      <View>
-        <TextInput
-          style={{
-            justifyContent: "center",
-            width: 340,
-            height: 60,
-            fontSize: 18,
-            margin: 5,
-          }}
-          mode="outlined"
-          label="Password"
-          theme={{
-            roundness: 15,
-            colors: {
-              primary: Colors.primaryColor,
-              nderlineColor: "blue",
-              placeholder: "#cecbce",
-              background: Colors.secondaryColor,
-            },
-          }}
-        />
-      </View>
-      <LinearGradient
-        colors={[Colors.primaryColor, "#6dd5ed"]}
-        style={{
-          padding: 15,
-          alignItems: "center",
-          borderRadius: 15,
-          height: 60,
-          width: 340,
-          marginTop: 90,
-        }}
-        start={{ x: 0, y: 0 }}
-        end={{ x: 1, y: 1 }}
-      >
-        <Text
-          style={{
-            backgroundColor: "transparent",
-            fontSize: 20,
-            color: "#fff",
-            fontFamily: "open-sans-bold",
-          }}
-        >
-          Login
+export default class SignupScreen extends Component {
+  constructor() {
+		super();
+		this.state = {
+			email: "",
+			password: "",
+			isLoading: false,
+		};
+  }
+  
+  onSignUp = async () => {
+  if (this.state.email && this.state.password) {
+    try {
+      const response = await firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.state.email, this.state.password);
+    } catch (error) {
+      if (error.code == "auth/email-already-in-use") {
+        alert("User already exists. Try again.");
+      }
+    }
+  } else {
+    alert("Please enter email and password");
+  }
+  };
+  
+  render() {
+    return (
+      <View style={styles.screen}>
+        <View style={styles.textsStyle}>
+          <Text style={styles.text1}>Create Account</Text>
+          <Text style={styles.text2}>Sign up to get started!</Text>
+        </View>
+        <View>
+          <TextInput
+            style={{
+              justifyContent: "center",
+              width: 340,
+              height: 60,
+              fontSize: 18,
+              margin: 5,
+            }}
+            mode="outlined"
+            label="Full Name"
+            theme={{
+              roundness: 15,
+              colors: {
+                primary: Colors.primaryColor,
+                nderlineColor: "blue",
+                placeholder: "#cecbce",
+                background: Colors.secondaryColor,
+              },
+            }}
+          />
+        </View>
+        <View>
+          <TextInput
+            style={{
+              justifyContent: "center",
+              width: 340,
+              height: 60,
+              fontSize: 18,
+              margin: 5,
+            }}
+            mode="outlined"
+            label="Email"
+            keyboardType='email-address'
+						onChangeText={(email) => this.setState({ email })}
+            theme={{
+              roundness: 15,
+              colors: {
+                primary: Colors.primaryColor,
+                nderlineColor: "blue",
+                placeholder: "#cecbce",
+                background: Colors.secondaryColor,
+              },
+            }}
+          />
+        </View>
+        <View>
+          <TextInput
+            style={{
+              justifyContent: "center",
+              width: 340,
+              height: 60,
+              fontSize: 18,
+              margin: 5,
+            }}
+            mode="outlined"
+            label="Password"
+            secureTextEntry
+						onChangeText={(password) => this.setState({ password })}
+            theme={{
+              roundness: 15,
+              colors: {
+                primary: Colors.primaryColor,
+                nderlineColor: "blue",
+                placeholder: "#cecbce",
+                background: Colors.secondaryColor,
+              },
+            }}
+          />
+        </View>
+        <TouchableOpacity onPress={this.onSignUp}>
+          <LinearGradient
+            colors={[Colors.primaryColor, "#6dd5ed"]}
+            style={{
+              padding: 15,
+              alignItems: "center",
+              borderRadius: 15,
+              height: 60,
+              width: 340,
+              marginTop: 90,
+            }}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1, y: 1 }}
+          >
+            <Text
+              style={{
+                backgroundColor: "transparent",
+                fontSize: 20,
+                color: "#fff",
+                fontFamily: "open-sans-bold",
+              }}
+            >
+              Login
         </Text>
-      </LinearGradient>
-      <View style={styles.textSignInContainer}>
-        <Text style={styles.text3}>I'm already a member,</Text>
-        <Button
-          title="Sign in"
-          titleStyle={{
-            fontSize: 15,
-            bottom: 11,
-            fontFamily: "open-sans",
-          }}
-          type="clear"
-          onPress={() => {
-            props.navigation.navigate({ routeName: "Login" });
-          }}
-        />
+          </LinearGradient>
+        </TouchableOpacity>
+        <View style={styles.textSignInContainer}>
+          <Text style={styles.text3}>I'm already a member,</Text>
+          <Button
+            title="Sign in"
+            titleStyle={{
+              fontSize: 15,
+              bottom: 11,
+              fontFamily: "open-sans",
+            }}
+            type="clear"
+            onPress={() => {
+              props.navigation.navigate({ routeName: "Login" });
+            }}
+          />
+        </View>
       </View>
-    </View>
-  );
+    );
+  }
 };
 
 const styles = StyleSheet.create({
@@ -151,5 +187,3 @@ const styles = StyleSheet.create({
     fontFamily: "open-sans",
   },
 });
-
-export default SignupScreen;
