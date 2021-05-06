ad359b1e5ee78914208d8c7af2730e6bf045eb7f
Shane Wilson <shanewilsonpro@gmail.com>
2021-05-05 22:36:50 -0700

    added mock video data  in flatlist and added functionality in search bar and it is able to search for video title

---
+++ src/data/dataTrending.js
@@ -0,0 +1,57 @@
+export default dataTrending = [
+  {
+    videoTitle: "Doctor",
+    url: "https://www.lifeprint.com/asl101/gifs/d/doctor-medic.gif",
+  },
+  {
+    videoTitle: "Computer",
+    url:
+      "https://i.pinimg.com/originals/f6/ab/b3/f6abb389c68bcbd7263aaab7d7fcb8e7.gif",
+  },
+  {
+    videoTitle: "Shower",
+    url: "https://www.lifeprint.com/asl101/gifs/s/shower.gif",
+  },
+  {
+    videoTitle: "Church",
+    url: "https://www.lifeprint.com/asl101/gifs/c/church.gif",
+  },
+  {
+    videoTitle: "Deaf",
+    url: "https://www.lifeprint.com/asl101/gifs/d/deaf.gif",
+  },
+  {
+    videoTitle: "Hard of Hearing",
+    url: "https://www.lifeprint.com/asl101/gifs/h/hard-of-hearing.gif",
+  },
+  {
+    videoTitle: "Hard",
+    url: "https://www.lifeprint.com/asl101/gifs/h/hard.gif",
+  },
+  {
+    videoTitle: "Have",
+    url: "https://www.lifeprint.com/asl101/gifs/h/have.gif",
+  },
+  {
+    videoTitle: "Need",
+    url:
+      "https://asluniversity.com/asl101/gifs/n/need-double-movement-side-view.gif",
+  },
+  {
+    videoTitle: "Minute",
+    url: "https://www.lifeprint.com/asl101/gifs/m/minute-1-min.gif",
+  },
+  {
+    videoTitle: "Fire",
+    url: "https://www.lifeprint.com/asl101/gifs/f/fire-terminante-1.gif",
+  },
+  {
+    videoTitle: "Oh I see",
+    url:
+      "https://i.pinimg.com/originals/e1/08/c7/e108c7b9c740ad2f37a00b1186367819.gif",
+  },
+  {
+    videoTitle: "Rain",
+    url: "https://www.lifeprint.com/asl101/gifs/r/rain.gif",
+  },
+];
