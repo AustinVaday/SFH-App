a47bd8d570a697cdaaed9720e56aa4506cd5133c
Shane Wilson <shanewilsonpro@gmail.com>
2021-06-08 22:34:35 -0700

    updated camera screen

--- src/components/spacer/spacer.components.js
+++ src/components/spacer/spacer.components.js
@@ -5,6 +5,7 @@ const sizeVariant = {
   small: 1,
   medium: 2,
   large: 3,
+  huge: 4,
 };
 
 const positionVariant = {
--- src/features/upload/screens/camera.screen.js
+++ src/features/upload/screens/camera.screen.js
@@ -1,10 +1,52 @@
 import React, { useState, useEffect, useRef } from "react";
-import { View, Text, SafeAreaView, StatusBar, StyleSheet } from "react-native";
-import { MaterialIcons } from "@expo/vector-icons";
+import { View } from "react-native";
 import { Camera } from "expo-camera";
 import { Video } from "expo-av";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import { IconButton } from "react-native-paper";
+import styled from "styled-components/native";
+
 import { colors } from "../../../infrastructure/theme/colors";
+import { Text } from "../../../components/typography/text.components";
+import { SafeArea } from "../../../components/utilities/safe-area.components";
+
+const PostCamera = styled(Camera)`
+  width: 100%;
+  height: 100%;
+  flex: 1;
+`;
+
+const CameraTopButtonsSection = styled.View`
+  padding-top: ${(props) => props.theme.space[5]};
+  padding-right: ${(props) => props.theme.space[2]};
+  padding-left: ${(props) => props.theme.space[2]};
+  flex-direction: row;
+`;
+
+const CameraButtons = styled.View`
+  flex: 1;
+  align-items: flex-end;
+`;
+
+const CameraBottomButtonsSection = styled.View`
+  padding-bottom: ${(props) => props.theme.space[5]};
+  padding-right: ${(props) => props.theme.space[2]};
+  padding-left: ${(props) => props.theme.space[2]};
+  flex-direction: row;
+  align-items: center;
+  margin-top: auto;
+  justify-content: space-evenly;
+`;
+
+const PreviewTitle = styled(Text)`
+  color: white;
+  font-size: 25px;
+`;
+
+const PreviewTopButtonsSection = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 export const CameraScreen = ({ navigation }) => {
   const [hasPermission, setHasPermission] = useState(null);
@@ -30,29 +72,25 @@ export const CameraScreen = ({ navigation }) => {
     return <Text>No access to camera</Text>;
   }
 
-  const _changeType = () => {
-    setType((prevState) => {
-      if (prevState.type === Camera.Constants.Type.back) {
-        return { type: Camera.Constants.Type.front };
-      } else {
-        return { type: Camera.Constants.Type.back };
-      }
-    });
+  const changeType = () => {
+    setType(
+      type === Camera.Constants.Type.back
+        ? Camera.Constants.Type.front
+        : Camera.Constants.Type.back
+    );
   };
 
-  const _changeFlash = () => {
-    setFlash((prevState) => {
-      if (prevState.flash === Camera.Constants.FlashMode.off) {
-        return { flash: Camera.Constants.FlashMode.on };
-      } else if (prevState.flash === Camera.Constants.FlashMode.on) {
-        return { flash: Camera.Constants.FlashMode.auto };
-      } else if (prevState.flash === Camera.Constants.FlashMode.auto) {
-        return { flash: Camera.Constants.FlashMode.off };
-      }
-    });
+  const changeFlash = () => {
+    if (flash === Camera.Constants.FlashMode.off) {
+      setFlash(Camera.Constants.FlashMode.on);
+    } else if (flash === Camera.Constants.FlashMode.on) {
+      setFlash(Camera.Constants.FlashMode.auto);
+    } else if (flash === Camera.Constants.FlashMode.auto) {
+      setFlash(Camera.Constants.FlashMode.off);
+    }
   };
 
-  const _takeVideo = async () => {
+  const takeVideo = async () => {
     if (cameraRef) {
       const takenVideo = await cameraRef.current.recordAsync();
       setVideo(takenVideo.uri);
@@ -60,208 +98,134 @@ export const CameraScreen = ({ navigation }) => {
     }
   };
 
-  const _rejectVideo = () => {
+  const rejectVideo = () => {
     setVideo(null);
     setVideoTaken(false);
   };
 
-  const _approveVideo = async () => {
-    const {
-      navigation: { navigate },
-    } = this.props;
-    navigate("Upload", { url: video });
+  const approveVideo = async () => {
+    navigation.navigate("Upload", { url: video });
     setVideo(null);
     setVideoTaken(false);
   };
 
   return (
-    <SafeAreaView style={styles.container}>
+    <>
       {videoTaken ? (
-        <>
-          <StatusBar
-            translucent
-            backgroundColor="white"
-            barStyle="light-content"
-          />
-          <View style={styles.videoActions}>
-            <TouchableOpacity onPressOut={_rejectVideo}>
-              <MaterialIcons name={"close"} size={40} color="white" />
-            </TouchableOpacity>
-            <Text style={styles.videoTitle}>Video</Text>
-            <TouchableOpacity onPressOut={_approveVideo}>
-              <MaterialIcons
-                name={"check-box"}
-                size={40}
-                color={colors.brand.primary}
-              />
-            </TouchableOpacity>
-          </View>
+        <SafeArea style={{ backgroundColor: "black" }}>
+          <PreviewTopButtonsSection>
+            <IconButton
+              size={40}
+              icon="close"
+              color="white"
+              onPress={rejectVideo}
+            />
+            <PreviewTitle>Preview</PreviewTitle>
+            <IconButton
+              size={40}
+              icon="checkbox-marked"
+              color={colors.brand.primary}
+              onPress={approveVideo}
+            />
+          </PreviewTopButtonsSection>
           <Video
             source={{ uri: video }}
             resizeMode="cover"
             isLooping
             useNativeControls
-            style={{ flex: 1, marginTop: 10 }}
+            style={{ flex: 1 }}
           />
-        </>
+        </SafeArea>
       ) : (
-        <Camera
+        <PostCamera
           type={type}
           flashMode={flash}
           ref={(camera) => (cameraRef.current = camera)}
-          style={styles.camera}
         >
           {!cameraIsRecording ? (
             <>
-              <View style={styles.actionsContainer}>
-                <View style={{ alignSelf: "flex-start" }}>
-                  <TouchableOpacity onPressOut={() => navigation.goBack()}>
-                    <MaterialIcons name={"close"} size={40} color="white" />
-                  </TouchableOpacity>
-                </View>
-                <View style={styles.action}>
-                  <TouchableOpacity onPressOut={_changeType}>
-                    <MaterialIcons
-                      name={
-                        type === Camera.Constants.Type.back
-                          ? "camera-front"
-                          : "camera-rear"
-                      }
-                      color="white"
-                      size={40}
-                    />
-                  </TouchableOpacity>
-                </View>
-                <View style={styles.action}>
-                  <TouchableOpacity onPressOut={_changeFlash}>
-                    {flash === Camera.Constants.FlashMode.off && (
-                      <MaterialIcons
-                        name={"flash-off"}
-                        color="white"
-                        size={40}
-                      />
-                    )}
-                    {flash === Camera.Constants.FlashMode.on && (
-                      <MaterialIcons
-                        name={"flash-on"}
-                        color="white"
-                        size={40}
-                      />
-                    )}
-                    {flash === Camera.Constants.FlashMode.auto && (
-                      <MaterialIcons
-                        name={"flash-auto"}
-                        color="white"
-                        size={40}
-                      />
-                    )}
-                  </TouchableOpacity>
-                </View>
-              </View>
-              <View style={styles.bottomButtonsContainer}>
-                <TouchableOpacity
-                  onPressOut={() => navigation.navigate("Library")}
-                >
-                  <MaterialIcons
-                    name={"video-library"}
-                    size={45}
-                    color="white"
-                  />
-                </TouchableOpacity>
-                <TouchableOpacity
-                  onPressOut={() => {
-                    if (cameraIsRecording) {
-                      setCameraIsRecording(false);
-                      cameraRef.current.stopRecording();
-                    } else {
-                      setCameraIsRecording(true);
-                      _takeVideo();
-                    }
+              <CameraTopButtonsSection>
+                <IconButton
+                  size={40}
+                  icon="close"
+                  color="white"
+                  onPress={() => {
+                    navigation.goBack();
                   }}
-                >
-                  <View style={styles.startbtn} />
-                </TouchableOpacity>
-                <View style={{ padding: 25 }} />
-              </View>
+                />
+                <CameraButtons>
+                  <IconButton
+                    size={40}
+                    icon={
+                      type === Camera.Constants.Type.back
+                        ? "camera-front"
+                        : "camera-rear"
+                    }
+                    color="white"
+                    onPress={changeType}
+                  />
+                  {flash === Camera.Constants.FlashMode.off && (
+                    <IconButton
+                      size={40}
+                      icon={"flash-off"}
+                      color="white"
+                      onPress={changeFlash}
+                    />
+                  )}
+                  {flash === Camera.Constants.FlashMode.on && (
+                    <IconButton
+                      size={40}
+                      icon={"flash"}
+                      color="white"
+                      onPress={changeFlash}
+                    />
+                  )}
+                  {flash === Camera.Constants.FlashMode.auto && (
+                    <IconButton
+                      size={40}
+                      icon={"flash-auto"}
+                      color="white"
+                      onPress={changeFlash}
+                    />
+                  )}
+                </CameraButtons>
+              </CameraTopButtonsSection>
+              <CameraBottomButtonsSection>
+                <IconButton
+                  size={45}
+                  icon={"filmstrip-box-multiple"}
+                  color="white"
+                  onPress={() => navigation.navigate("Library")}
+                />
+                <IconButton
+                  size={70}
+                  icon={"record"}
+                  color="red"
+                  style={{ borderWidth: 10, borderColor: "white" }}
+                  onPress={() => {
+                    setCameraIsRecording(true);
+                    takeVideo();
+                  }}
+                />
+                <View style={{ padding: 40 }} />
+              </CameraBottomButtonsSection>
             </>
           ) : (
-            <View style={styles.bottomButtonsContainer}>
-              <TouchableOpacity
-                onPressOut={() => {
-                  if (cameraIsRecording) {
-                    setCameraIsRecording(false);
-                    cameraRef.current.stopRecording();
-                  } else {
-                    setCameraIsRecording(true);
-                    _takeVideo();
-                  }
+            <CameraBottomButtonsSection>
+              <IconButton
+                size={70}
+                icon={"stop"}
+                color="red"
+                style={{ borderWidth: 10, borderColor: "white" }}
+                onPress={() => {
+                  setCameraIsRecording(false);
+                  cameraRef.current.stopRecording();
                 }}
-              >
-                <View style={styles.stopbtn} />
-              </TouchableOpacity>
-            </View>
+              />
+            </CameraBottomButtonsSection>
           )}
-        </Camera>
+        </PostCamera>
       )}
-    </SafeAreaView>
+    </>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "black",
-  },
-  camera: {
-    flex: 1,
-  },
-  videoTitle: {
-    color: "white",
-    fontSize: 25,
-  },
-  btnContainer: {
-    flex: 1,
-    justifyContent: "space-between",
-    alignItems: "center",
-    flexDirection: "row",
-    margin: 30,
-  },
-  startbtn: {
-    width: 100,
-    height: 100,
-    backgroundColor: "red",
-    borderColor: "#bbb",
-    borderWidth: 15,
-    borderRadius: 50,
-  },
-  stopbtn: {
-    width: 100,
-    height: 100,
-    backgroundColor: "#bbb",
-    borderColor: "red",
-    borderWidth: 15,
-    borderRadius: 50,
-  },
-  action: {
-    backgroundColor: "transparent",
-    marginBottom: 20,
-    alignSelf: "flex-end",
-    bottom: 35,
-  },
-  actionsContainer: {
-    margin: 15,
-  },
-  bottomButtonsContainer: {
-    marginTop: "auto",
-    marginBottom: 40,
-    alignItems: "center",
-    flexDirection: "row",
-    justifyContent: "space-around",
-  },
-  videoActions: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    marginHorizontal: 20,
-    alignItems: "center",
-  },
-});
