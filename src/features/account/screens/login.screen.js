3b28154374ae8f6c71d11599b60e46c84a41e5e2
Shane Wilson <shanewilsonpro@gmail.com>
2021-06-15 19:13:09 -0700

    installed react native gradient buttons and updated login screen

--- src/features/account/screens/login.screen.js
+++ src/features/account/screens/login.screen.js
@@ -1,34 +1,83 @@
-import React, { useEffect } from "react";
+import React, { useContext } from "react";
+import { ScrollView } from "react-native";
 import {
-  View,
-  Text,
-  StyleSheet,
+  TextInput,
+  HelperText,
   Button,
-  SafeAreaView,
-  TouchableOpacity,
-} from "react-native";
-import { colors } from "../../../infrastructure/theme/colors";
-import { TextInput } from "react-native-paper";
-import * as firebase from "firebase/app";
-import "firebase/auth";
-import { LinearGradient } from "expo-linear-gradient";
+  ActivityIndicator,
+} from "react-native-paper";
+import GradientButton from "react-native-gradient-buttons";
 import { Formik } from "formik";
-import * as Yup from "yup";
-import NetInfo from "@react-native-community/netinfo";
-import DropdownAlert from "react-native-dropdownalert";
 
-const validationSchema = Yup.object().shape({
-  email: Yup.string()
+import * as yup from "yup";
+import NetInfo from "@react-native-community/netinfo";
+import styled from "styled-components/native";
+
+import { colors } from "../../../infrastructure/theme/colors";
+import { SafeArea } from "../../../components/utilities/safe-area.components";
+import { Text } from "../../../components/typography/text.components";
+
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
+
+const TopTextSection = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const TextInputsSection = styled.View`
+  padding-top: ${(props) => props.theme.space[5]};
+  padding-left: ${(props) => props.theme.space[3]};
+  padding-right: ${(props) => props.theme.space[3]};
+`;
+
+const ForgotPasswordSection = styled.View`
+  align-items: flex-end;
+`;
+
+const LoginButtonSection = styled.View`
+  padding-top: ${(props) => props.theme.space[5]};
+  padding-left: ${(props) => props.theme.space[4]};
+  padding-right: ${(props) => props.theme.space[4]};
+`;
+
+const SignUpSection = styled.View`
+  align-items: center;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+const LoginText = styled(Text)`
+  color: white;
+  font-family: ${(props) => props.theme.fonts.body_medium};
+  font-size: ${(props) => props.theme.fontSizes.title};
+`;
+
+const WelcomeText = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.body_bold};
+  font-size: ${(props) => props.theme.fontSizes.h2};
+`;
+
+const ContinueLoginText = styled(Text)`
+  color: #bdc3d4;
+  font-size: ${(props) => props.theme.fontSizes.h4};
+`;
+
+const NewUserText = styled(Text)`
+  font-size: ${(props) => props.theme.fontSizes.body};
+`;
+
+const validationSchema = yup.object().shape({
+  email: yup
+    .string()
     .label("Email")
     .email("Enter a valid email")
     .required("Please enter a registered email"),
-  password: Yup.string()
+  password: yup
+    .string()
     .label("Password")
     .required()
     .min(5, "Password should be at least 5 characters "),
 });
 
-// TODO create the network information based on its quality and implement the type of modals for password
 // create the Network Information
 const unsubscribe = NetInfo.addEventListener((state) => {
   console.log("Connection type", state.type);
@@ -37,63 +86,29 @@ const unsubscribe = NetInfo.addEventListener((state) => {
 
 // to unsubscribe to these update, just use:
 unsubscribe();
-// TODO END -> network information
 
 export const LoginScreen = ({ navigation }) => {
-  // create the component for alert (dropdown)
-  // const _fetchData = async () => {
-  //   try {
-  //     // alertWithType parameters: type, title, message, payload, interval.
-  //     // payload object that includes a source property overrides the image source prop. (optional: object)
-  //     // interval takes precedence over the closeInterval prop. (optional: number)
-  //     this.dropDownAlertRef.alertWithType("info", "Info", "Start fetch data.");
-  //     await fetch("https://httpbin.org/get");
-  //     this.dropDownAlertRef.alertWithType(
-  //       "success",
-  //       "Success",
-  //       "Finish fetch data"
-  //     );
-  //   } catch (error) {
-  //     this.dropDownAlertRef.alertWithType("error", "Error", error);
-  //   }
-  // };
+  const { onLogin, error, isLoading } = useContext(AuthenticationContext);
 
   const onSignIn = async (values, actions) => {
     const { email, password } = values;
     try {
-      const response = await firebase
-        .auth()
-        .signInWithEmailAndPassword(email, password);
-      if (response.user) {
-        navigation.navigate("Home");
-      }
+      onLogin(email, password);
     } catch (error) {
-      // this code will hold the error handler to avoid the error message: "the password is invalid or the user does have a password"
-      actions.setFieldError("general", error.message); //console.error(error);
+      // this code will hold the error handler to avoid the error message: "the
+      // password is invalid or the user does have a password"
+      actions.setFieldError("general", error.message);
     } finally {
       actions.setSubmitting(false);
     }
   };
 
-  // TODO create the function to cleanup the memory to avoid memory leak.
-  /* 
-    ! DOCUMENTATION: Using a hook for useEffect() that adds the ability to perform side effects from a function component
-    ! Same above. It servers the same purpose as componentDidMount, componentDidUpdate, and componentWillUnmount in classes. 
-    ! In my progress, I am trying to figure out why it still keeps componentMount, not using effect hooks.
-    * Highlight of my progress, I complete the task for modals in the terminal as comment. However, I still work on the modals as pop in the actual phone
-  */
-
-  // useEffect(() => {
-  //   _fetchData();
-  //   const subscription = source.subscribe();
-  //   console.log("Here");
-  //   return () => {
-  //     subscription.unsubscribe();
-  //   };
-  // }, [source]);
-
   return (
-    <SafeAreaView style={styles.screen}>
+    <SafeArea>
+      <TopTextSection>
+        <WelcomeText>Welcome</WelcomeText>
+        <ContinueLoginText>Log in to continue!</ContinueLoginText>
+      </TopTextSection>
       <Formik
         initialValues={{ email: "", password: "" }}
         onSubmit={(values, actions) => onSignIn(values, actions)}
@@ -107,25 +122,8 @@ export const LoginScreen = ({ navigation }) => {
           touched,
           handleBlur,
         }) => (
-          <View>
-            <View style={{ marginTop: 10 }}>
-              <Text
-                style={{
-                  fontSize: 35,
-                }}
-              >
-                Welcome
-              </Text>
-              <Text
-                style={{
-                  fontSize: 25,
-                  color: "#bdc3d4",
-                }}
-              >
-                Log in to continue!
-              </Text>
-            </View>
-            <View style={styles.inputContainer}>
+          <ScrollView scrollEnabled={false}>
+            <TextInputsSection>
               <TextInput
                 mode="outlined"
                 label="Email"
@@ -139,13 +137,13 @@ export const LoginScreen = ({ navigation }) => {
                     primary: colors.brand.primary,
                     nderlineColor: "blue",
                     placeholder: "#cecbce",
-                    background: colors.brand.secondary,
+                    background: "white",
                   },
                 }}
               />
-              <Text style={{ color: "red" }}>
+              <HelperText type="error" visible={errors}>
                 {touched.email && errors.email}
-              </Text>
+              </HelperText>
               <TextInput
                 mode="outlined"
                 label="Password"
@@ -159,100 +157,61 @@ export const LoginScreen = ({ navigation }) => {
                     primary: colors.brand.primary,
                     nderlineColor: "blue",
                     placeholder: "#cecbce",
-                    background: colors.brand.secondary,
+                    background: "white",
                   },
                 }}
               />
-              <Text style={{ color: "red" }}>
+              <HelperText type="error" visible={errors}>
                 {touched.password && errors.password}
-              </Text>
-              <Text style={{ color: "red" }}>{errors.general}</Text>
-              <TouchableOpacity
-                style={{ marginLeft: 220 }}
-                onPress={() => {
-                  navigation.navigate("ForgotPassword");
-                }}
-              >
-                <Text>Forgot password?</Text>
-              </TouchableOpacity>
-
-              <View style={{ marginTop: 80 }}>
-                <TouchableOpacity onPress={handleSubmit}>
-                  <LinearGradient
-                    colors={[colors.brand.primary, "#6dd5ed"]}
-                    style={{
-                      padding: 15,
-                      alignItems: "center",
-                      borderRadius: 15,
-                      height: 60,
-                    }}
-                    start={{ x: 0, y: 0 }}
-                    end={{ x: 1, y: 1 }}
-                  >
-                    <Text
-                      style={{
-                        backgroundColor: "transparent",
-                        fontSize: 20,
-                        color: "#fff",
-                      }}
-                    >
-                      Login
-                    </Text>
-                  </LinearGradient>
-                </TouchableOpacity>
-              </View>
-            </View>
-            <View style={styles.signupButtonContainer}>
-              <Text
-                style={{
-                  fontSize: 16,
-                  marginTop: 8,
-                }}
-              >
-                New user?
-              </Text>
-              <Button
-                title="Sign up"
-                style={{
-                  fontSize: 16,
-                }}
-                onPress={() => {
-                  navigation.navigate({ routeName: "Register" });
-                }}
-              />
-            </View>
-          </View>
+              </HelperText>
+              <ForgotPasswordSection>
+                <Button
+                  color="white"
+                  labelStyle={{
+                    fontSize: 10,
+                    color: "black",
+                  }}
+                  onPress={() => {
+                    navigation.navigate("ForgotPassword");
+                  }}
+                >
+                  Forgot password?
+                </Button>
+              </ForgotPasswordSection>
+            </TextInputsSection>
+            <LoginButtonSection>
+              {!isLoading ? (
+                <GradientButton
+                  text={<LoginText>Login</LoginText>}
+                  gradientBegin={colors.brand.primary}
+                  gradientEnd="#6dd5ed"
+                  gradientDirection="vertical"
+                  height={60}
+                  radius={30}
+                  onPressAction={handleSubmit}
+                />
+              ) : (
+                <ActivityIndicator animating={true} color={Colors.blue300} />
+              )}
+            </LoginButtonSection>
+          </ScrollView>
         )}
       </Formik>
-    </SafeAreaView>
+      <SignUpSection>
+        <NewUserText>New user?</NewUserText>
+        <Button
+          color="white"
+          labelStyle={{
+            fontSize: 13,
+            color: colors.brand.primary,
+          }}
+          onPress={() => {
+            navigation.navigate("Register");
+          }}
+        >
+          Sign up
+        </Button>
+      </SignUpSection>
+    </SafeArea>
   );
 };
-
-// FEEDBACK: remove the logo on the login page.
-// below the snippet, its only for sfh logo. (keep it in just a case)
-/* <View style={{ marginBottom: 20 }}>
-	<Image
-		source={require("../assets/sfh-logo-white-removebg.png")}
-		style={{ width: 150, height: 125 }}
-	/>
-</View>; */
-
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: colors.brand.secondary,
-    padding: 20,
-  },
-  inputContainer: {
-    width: 340,
-    fontSize: 18,
-    marginTop: 190,
-  },
-  signupButtonContainer: {
-    marginTop: 170,
-    marginLeft: 90,
-    flexDirection: "row",
-  },
-});
